29f110473a316e37eacdb9b58834fff70da3c792
romusadd <romusadd@gmail.com>
2024-07-05 05:46:52 +0000

    first

--- src/config/site.js
+++ src/config/site.js
@@ -1,6 +1,6 @@
 export function constructMetadata({
-  title = "Abdullah Agency ",
-  description = "Abdullah Agency is an open-source website.",
+  title = "Inovisia ",
+  description = "Inovisa is an open-source website.",
   image = "/agency.PNG",
   icons = "/favicon.ico",
   noIndex = false,
